099f3415fbdbdbb1f9eef2563081d9c14fb70f05
Victor Matthews <victormmatthews@gmail.com>
2021-06-25 13:50:37 -0600

    Remove unused constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,31 +1,21 @@
 export class Constants {
     public static BOT = 'Bot';
-    public static CALLED_COMMAND = 'Called command: ';
     public static CLIENT = 'Client';
-    public static CLOSE_PAR = ')';
     public static CLOSE_TAG = '> ';
-    public static CODE = '```';
-    public static COMMAND_ERROR = 'there was an error trying to execute that command!';
-    public static HELP = 'help';
-    public static HELP_DEF = 'This is a list of commands that can be used by this bot.';
-    public static LINE_RETURN = '\n';
+    public static COMMAND = 'command';
+    public static COMMAND_RESPONDER = 'CommandResponder';
+    public static DELETE = 'delete';
+    public static GUILD_ID = 'GuildId';
     public static LISTENING = 'Listening to request';
     public static LOGGED_IN_AS = 'We have logged in as ';
     public static MESSAGE = 'message';
     public static MESSAGE_RECEIVED = 'Message received! Contents: ';
-    public static MESSAGE_SEND = 'Successfully sent message: ';
+    public static MESSAGE_RESPONDER = 'MessageResponder';
     public static OH_NO = 'Oh no! ';
-    public static OPEN_PAR = '(';
     public static OPEN_USER_TAG = '<@!';
     public static PING = 'ping';
+    public static POST = 'post';
     public static QUOTE = '"';
     public static READY = 'ready';
-    public static SPACE = ' ';
     public static TOKEN = 'Token';
-    public static MESSAGE_RESPONDER = 'MessageResponder';
-    public static COMMAND_RESPONDER = 'CommandResponder';
-    public static POST = 'post';
-    public static DELETE = 'delete';
-    public static COMMAND = 'command';
-    public static GUILD_ID = 'GuildId';
 }
